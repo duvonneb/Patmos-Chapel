790389fc8061cd69fbfd6311dfea797f8f636fb4
duvonneb <duvonneb@gmail.com>
2016-12-18 19:16:58 -0500

    Initial Commit

---
+++ Patmos Chapel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patmos Chapel
+//
+//  Created by Duvonne Berry on 12/18/16.
+//  Copyright © 2016 Duvonne Berry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Patmos Chapel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patmos Chapel
+//
+//  Created by Duvonne Berry on 12/18/16.
+//  Copyright © 2016 Duvonne Berry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
